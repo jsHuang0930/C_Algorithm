bb0dad0b5a08fc83534fc4fdeebdd2af35835283
jsHuang_0930 <13401495397@163.com>
2020-11-16 17:23:53 +0800

    null

---
+++ .history/Leetcode/2.两数相加_20201116170818.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=cpp
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+
+ * };
+ */
+class Solution {
+  public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *l;
+        ListNode *p = l;
+        while (l1.next != null && l2.next != null) {
+            p.val=l1.val
+        }
+    }
+};
+// @lc code=end
---
+++ .history/Leetcode/2.两数相加_20201116172132.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=cpp
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+
+class Solution {
+  public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *l;
+        ListNode *p = l;
+        while (l1->next != null && l2->next != null) {
+            p.val=l1->val+l2->val;
+            if (p->val>=10)
+            {
+                p->next=p->next-10
+            }
+            
+            p=p->next;
+            l1=l1->next;
+            l2=l2->next;
+            
+        }
+    }
+};
+// @lc code=end
