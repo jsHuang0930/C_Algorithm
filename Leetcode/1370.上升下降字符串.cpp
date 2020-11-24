f16028bf46894b48154d9657bce371e31a349407
jsHuang0930 <13401495397@163.com>
2020-11-25 03:36:33 +0800

    null

---
+++ Leetcode/1370.上升下降字符串.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=1370 lang=cpp
+ *
+ * [1370] 上升下降字符串
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    string sortString(string s)
+    {
+        vector<int> num(26);
+        for (char &ch : s)
+        {
+            num[ch - 'a']++;
+        }
+
+        string ret;
+        while (ret.length() < s.length())
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                if (num[i])
+                {
+                    ret.push_back(i + 'a');
+                    num[i]--;
+                }
+            }
+            for (int i = 25; i >= 0; i--)
+            {
+                if (num[i])
+                {
+                    ret.push_back(i + 'a');
+                    num[i]--;
+                }
+            }
+        }
+        return ret;
+    }
+};
+
+// @lc code=end
