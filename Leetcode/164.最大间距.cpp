52430058d36e8ab5d961a3c3fc35066d28daf4ae
jsHuang_0930 <13401495397@163.com>
2020-11-26 11:48:08 +0800

    2020.11.26

--- Leetcode/164.最大间距.cpp
+++ Leetcode/164.最大间距.cpp
@@ -7,21 +7,25 @@
 using namespace std;
 // @lc code=start
 class Solution {
-public:
-    int maximumGap(vector<int>& nums) {
-        int max_dif=0;
+  public:
+    int maximumGap(vector<int> &nums) {
+        int max_dif = 0; //最大差值
         vector<int>::iterator it;
-        if (nums.size()<2)
-        {
+        if (nums.size() < 2) { //数组元素个数小于 2，因此返回 0
             return 0;
         }
-        while (nums.size()>0)
-        {
-            it=nums.max_element();
-            
+        it = min_element(nums.begin(), nums.end()); // tmp为剩余nums中的最小值
+        int tmp = *it;
+        nums.erase(it);
+        while (nums.size() > 0) {
+            it = min_element(nums.begin(), nums.end());
+            if (max_dif < *it - tmp) {
+                max_dif = *it - tmp;
+            }
+            tmp = *it;
+            nums.erase(it);
         }
-        
+        return max_dif;
     }
 };
 // @lc code=end
-
--- Leetcode/9.回文数.cpp
+++ Leetcode/9.回文数.cpp
@@ -20,7 +20,6 @@ public:
         }
         return true;
         
-        
     }
 };
 // @lc code=end
