802cee229e0880a8e8cca72e72b3068354284652
jsHuang0930 <13401495397@163.com>
2020-11-30 00:12:45 +0800

    2020.11.20

---
+++ Leetcode/493.翻转对.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=493 lang=cpp
+ *
+ * [493] 翻转对
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution
+{//运行时间受限 Time Limit Exceeded
+public:
+    int reversePairs(vector<int> &nums)
+    {
+        vector<long long> numsdouble;
+        int count = 0;
+        if (nums.size() < 2)
+            return 0;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            numsdouble.push_back((long long)nums[i] * 2);//强制类型转换
+        }
+        for (int j = 0; j < nums.size() - 1; j++)
+        {
+            for (int k = j; k < numsdouble.size(); k++)
+            {
+                if (nums[j] > numsdouble[k])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+// @lc code=end
---
+++ Leetcode/976.三角形的最大周长.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=976 lang=cpp
+ *
+ * [976] 三角形的最大周长
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+bool cmp(int a,int b){
+    return a>b;
+}
+class Solution {
+public:
+    int largestPerimeter(vector<int>& A) {
+        sort(A.begin(),A.end(),cmp);
+        for (int i = 0; i < A.size()-2; i++)
+        {
+            if(A[i+2]+A[i+1]>A[i])
+                return A[i+2]+A[i+1]+A[i];
+            
+        }
+        return 0;
+    }
+};
+// @lc code=end
+
