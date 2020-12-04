71947d00e0d8729a0fb83f58f08497a7f1c83b04
jsHuang0930 <13401495397@163.com>
2020-12-04 23:19:27 +0800

    2020.12.4

---
+++ Leetcode/659.分割数组为连续子序列.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=659 lang=cpp
+ *
+ * [659] 分割数组为连续子序列
+ * 2020.12.4
+ */
+#include <bits\stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        vector<int> Nums[3333];
+        for(int i:nums){
+            
+        }
+    }
+};
+// @lc code=end
+
